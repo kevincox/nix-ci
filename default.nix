ba6377dc41defd505aa39e40947ed7f4936a395a
Kevin Cox <kevincox@kevincox.ca>
2016-03-06 10:14:58 -0500

    Add empty directory source.

--- default.nix
+++ default.nix
@@ -5,6 +5,8 @@ stdenv.mkDerivation {
 	
 	buildInputs = [ ruby ];
 	
+	src = builtins.filterSource (name: type: false) ./.;
+	
 	installPhase = ''
 		ruby -e 'File.write ARGV[0], "content"' "$out"
 	'';
