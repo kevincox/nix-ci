9cedccb9582c8d5f098466f9aff2f60c32663d3d
Kevin Cox <kevincox@kevincox.ca>
2016-03-06 10:20:24 -0500

    Make installing nix.conf optional.

--- nix-ci.sh
+++ nix-ci.sh
@@ -24,7 +24,7 @@ if [ '!' -d /nix ]; then
 	source ~/.nix-profile/etc/profile.d/nix.sh
 fi
 
-sudo install -Dm644 secrets/nix.conf /etc/nix.conf
+[ -f secret/nix.conf ] && sudo install -Dm644 secrets/nix.conf /etc/nix.conf
 
 nix-build "${flags[@]}" -A ${BUILD_ATTR:-all}
 
